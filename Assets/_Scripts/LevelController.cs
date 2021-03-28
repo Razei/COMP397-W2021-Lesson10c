8cca8022bd712ba1de2ffcb9bb086cfd16355c27
Razei <razeixyz@gmail.com>
2021-03-27 22:28:58 -0400

    First draft of procedural world

--- Assets/_Scripts/LevelController.cs
+++ Assets/_Scripts/LevelController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class LevelController : MonoBehaviour
 {
@@ -9,28 +10,50 @@ public class LevelController : MonoBehaviour
     public float tileLength;
     public List<GameObject> tilePrefabs;
     public List<GameObject> activeTiles;
+    public GameObject startTile;
+
+    [Header("Navigation")]
+    private NavMeshSurface surface;
+
+    void Awake()
+    {
+        surface = GetComponent<NavMeshSurface>();
+        BuildWorld();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int width = 0; width < tileWidth; width++)
-        {
-            for (int length = 0; length < tileLength; length++)
-            {
-                int randomTileIndex = Random.Range(0, tilePrefabs.Count);
-                Vector3 randomTilePosition = new Vector3(width * 16, 0.0f, length * 16);
-                float randomTileRotation = Random.Range(0, 4) * 90.0f;
-                GameObject randomTile = Instantiate(tilePrefabs[randomTileIndex], randomTilePosition, Quaternion.Euler(0.0f, randomTileRotation, 0.0f));
-
-                randomTile.transform.parent = this.transform;
-                activeTiles.Add(randomTile);
-            }
-        }
+        surface.BuildNavMesh();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void BuildWorld()
+    {
+        for (int width = 0; width < tileWidth; width++)
+        {
+            for (int length = 0; length < tileLength; length++)
+            {
+                if (activeTiles.Count < 1)
+                {
+                    activeTiles.Add(startTile);
+                    continue;
+                } 
+                else
+                {
+                    int randomTileIndex = Random.Range(0, tilePrefabs.Count);
+                    Vector3 randomTilePosition = new Vector3(width * 16, 0.0f, length * 16);
+                    float randomTileRotation = Random.Range(0, 4) * 90.0f;
+                    GameObject randomTile = Instantiate(tilePrefabs[randomTileIndex], randomTilePosition, Quaternion.Euler(0.0f, randomTileRotation, 0.0f));
+
+                    randomTile.transform.parent = this.transform;
+                    activeTiles.Add(randomTile);
+                }
+            }
+        }
     }
 }
